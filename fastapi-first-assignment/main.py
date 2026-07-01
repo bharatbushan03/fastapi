9c3884d32e36498c0b4dbbdd5158fe9b94edf58c
bharat <bharatbushan5320@gmail.com>
2026-07-01 22:52:23 +0530

    added day1 assignment

---
+++ fastapi-first-assignment/main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def welcome():
+    return {
+        "message": "Welcome to my first FastAPI assignment"
+     }
+
+@app.get('/about')
+def about():
+    return {
+        "student_name": "Bharat Lashotra",
+        "course_name": "FastAPI",
+        "topic": "First API Assignment",
+        "status": "Learning"
+    }
+
+@app.get('/trainer')
+def get_trainer():
+    return {
+        "name": "Hemanth",
+        "role": "Trainer",
+        "subject": "FastAPI"
+    }
+
+@app.get('/courses')
+def get_courses():
+    return [
+        {
+            "id": 1,
+            "name": "Python Basics",
+            "duration": "1 week"
+        }, 
+        {
+            "id": 2,
+            "name": "FastAPI",
+            "duration": "2 weeks"
+        }, 
+        {
+            "id": 3,
+            "name": "SQL Basics",
+            "duration": "1 week"
+        }
+
+    ]
+@app.get('/students')
+def get_students():
+    return [
+        {
+            "id": 1,
+            "namw": "Akhil",
+            "course": "Python",
+            "city": "Hyderabad"
+        },
+        {
+            "id": 2,
+            "name": "Sai",
+            "course": "FastAPI",
+            "city": "Vijayawada"
+        }
+    ]
+
+@app.get('/college')
+def get_college():
+    return {
+        "college_name": "MIET",
+        "location": "Jammu",
+        "department": "CSE",
+        "current_module": "FastAPI Basics"
+    }
+
+
+@app.get('/technologies')
+def get_technology():
+    return [
+        "Python",
+        "FastAPI",
+        "JSON",
+        "HTTP",
+        "Rest API"
+    ]
