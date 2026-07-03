e55a327c47f16c0dec11125bf5b154b3dabb176e
bharat <bharatbushan5320@gmail.com>
2026-07-03 15:08:57 +0530

    added static route before dynamic route

--- day2/main.py
+++ day2/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 students = [
-    {"id": 1, "name": "Akhil", "course": "Python", "city": "Hyderabad"},
-    {"id": 2, "name": "Sai", "course": "FastAPI", "city": "Vijaywada"},
-    {"id": 3, "name": "Ravi", "course": "SQL", "city": "Guntur"},
-    {"id": 4, "name": "Kiran", "course": "Python", "city": "Vizag"},
-    {"id": 5, "name": "Meena", "course": "FastAPI", "city": "Chennai"}
+    {"id": 1, "name": "Akhil", "course": "Python",  "city": "Hyderabad"},
+    {"id": 2, "name": "Sai",   "course": "FastAPI", "city": "Vijaywada"},
+    {"id": 3, "name": "Ravi",  "course": "SQL",     "city": "Guntur"   },
+    {"id": 4, "name": "Kiran", "course": "Python",  "city": "Vizag"    },
+    {"id": 5, "name": "Meena", "course": "FastAPI", "city": "Chennai"  }
 ]
 
 @app.get('/')
@@ -18,9 +18,16 @@ def home():
 def get_students():
     return students
 
+@app.get("/students/topper")
+def get_topper_student():
+    return {
+        "name": "Akhil",
+        "rank": "topper"
+    }
+
 @app.get("/students/{student_id}")
 def get_student_by_id(student_id: int):
     for student in students:
         if student["id"] == student_id:
             return student
-    return {"error": "Student not found"}
+    raise HTTPException(status_code = 404, detail = "Student not found")
