473dff41315d9aad1cee0cea2f687691f9eaa9cc
bharat <bharatbushan5320@gmail.com>
2026-07-03 14:42:57 +0530

    added day2 main file

---
+++ day2/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+students = [
+    {"id": 1, "name": "Akhil", "course": "Python", "city": "Hyderabad"},
+    {"id": 2, "name": "Sai", "course": "FastAPI", "city": "Vijaywada"},
+    {"id": 3, "name": "Ravi", "course": "SQL", "city": "Guntur"},
+    {"id": 4, "name": "Kiran", "course": "Python", "city": "Vizag"},
+    {"id": 5, "name": "Meena", "course": "FastAPI", "city": "Chennai"}
+]
+
+@app.get('/')
+def home():
+    return {"message": "Welcome to the Student API"}
+
+@app.get("/students")
+def get_students():
+    return students
+
+@app.get("/students/{student_id}")
+def get_student_by_id(student_id: int):
+    for student in students:
+        if student["id"] == student_id:
+            return student
+    return {"error": "Student not found"}
